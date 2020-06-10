1b0fa7761ed56df8492936cee54141f185cac8d8
Bill Dami <bdami@gavant.com>
2020-06-10 11:13:43 -0400

    generate skeleton app

---
+++ pep/app/pods/login/route.ts
@@ -0,0 +1,8 @@
+import PageLayout from 'pep/mixins/page-layout';
+import Route from '@ember/routing/route';
+import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
+
+export default class Login extends PageLayout(Route.extend(UnauthenticatedRouteMixin)) {
+    classNames = ['login'];
+    routeIfAlreadyAuthenticated = 'landing-route-here';
+}
---
+++ pep/types/global.d.ts
@@ -0,0 +1,6 @@
+// Types for compiled templates
+declare module 'pep/templates/*' {
+    import { TemplateFactory } from 'htmlbars-inline-precompile';
+    const tmpl: TemplateFactory;
+    export default tmpl;
+}
