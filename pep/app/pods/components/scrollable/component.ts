89e001a61605d4b74a6f3a30b5f1b994ecd24744
Bill Dami <billdami@gmail.com>
2020-06-10 20:40:14 -0400

    scrollables

--- pep/app/pods/components/scrollable/component.ts
+++ pep/app/pods/components/scrollable/component.ts
@@ -1,11 +1,17 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import PerfectScrollbar from 'perfect-scrollbar';
 
 interface ScrollableArgs {}
 
 export default class Scrollable extends Component<ScrollableArgs> {
     @action
-    initScrollable() {
-        console.log('insert!');
+    initScrollable(element) {
+        const ps = new PerfectScrollbar(element, {
+            //@see https://github.com/mdbootstrap/perfect-scrollbar#options
+            wheelSpeed: 1
+        });
+
+        this.ps = ps;
     }
 }
