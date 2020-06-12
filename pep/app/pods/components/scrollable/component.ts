76dbce7d5c69e0e8f9ba86ac6c8e8babb81f0fd3
Bill Dami <bdami@gavant.com>
2020-06-12 09:43:34 -0400

    tweaks to sidebar/scrollables

--- pep/app/pods/components/scrollable/component.ts
+++ pep/app/pods/components/scrollable/component.ts
@@ -1,17 +1,56 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import PerfectScrollbar from 'perfect-scrollbar';
+import { inject as service } from '@ember/service';
 
 interface ScrollableArgs {}
 
 export default class Scrollable extends Component<ScrollableArgs> {
-    @action
-    initScrollable(element) {
-        const ps = new PerfectScrollbar(element, {
-            //@see https://github.com/mdbootstrap/perfect-scrollbar#options
-            wheelSpeed: 1
-        });
+    @service media;
 
-        this.ps = ps;
+    ps = null;
+    scrollElement = null;
+
+    constructor(owner, args) {
+        super(owner, args);
+        this.handleMediaChange();
+        this.media.on('mediaChanged', this, this.handleMediaChange);
+    }
+
+    setup(element) {
+        if (element && !this.ps) {
+            console.log('setup!');
+            const ps = new PerfectScrollbar(element, {
+                //@see https://github.com/mdbootstrap/perfect-scrollbar#options
+                wheelSpeed: 1
+            });
+
+            this.ps = ps;
+            this.scrollElement = element;
+        }
+    }
+
+    teardown() {
+        if (this.ps) {
+            console.log('teardown!');
+            this.ps.destroy();
+            this.ps = null;
+        }
+    }
+
+    handleMediaChange() {
+        //disable perfect-scrollbar in mobile as it doesnt support momentum scrolling
+        if (this.media.isMobile || this.media.isTablet) {
+            this.teardown();
+        } else {
+            this.setup(this.scrollElement);
+        }
+    }
+
+    @action
+    onElementInsert(element) {
+        if (!this.media.isMobile && !this.media.isTablet) {
+            this.setup(element);
+        }
     }
 }
