424a2b0f654fd04ea75623ab47e2304611c89858
Adam Baker <abaker@gavant.com>
2020-08-06 12:13:27 -0400

    Adding type fixes

---
+++ pep/app/helpers/types.ts
@@ -0,0 +1,9 @@
+export const guard = <T>(itemToCheck: any, propertyNames: Array<keyof T> | keyof T): itemToCheck is T => {
+    return Array.isArray(propertyNames)
+        ? Object.keys(itemToCheck as T).some((key) => propertyNames.indexOf(key as keyof T) >= 0)
+        : (itemToCheck as T)[propertyNames as keyof T] !== undefined;
+};
+
+export const pluck = <T, K extends keyof T>(o: T, propertyNames: K[] | K): T[K][] | T[K] => {
+    return Array.isArray(propertyNames) ? propertyNames.map((n) => o[n]) : o[propertyNames];
+};
