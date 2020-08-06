f83836a8ee4f545338d5c8e7728b34dc2c289cd8
Adam Baker <abaker@gavant.com>
2020-08-06 14:04:46 -0400

    MR fixes

--- pep/app/utils/types.ts
+++ pep/app/utils/types.ts
@@ -1,9 +1,26 @@
+/**
+ * Generic type guard
+ *
+ * @template T
+ * @param {*} itemToCheck
+ * @param {(Array<keyof T> | keyof T)} propertyNames
+ * @returns {itemToCheck is T}
+ */
 export const guard = <T>(itemToCheck: any, propertyNames: Array<keyof T> | keyof T): itemToCheck is T => {
     return Array.isArray(propertyNames)
         ? Object.keys(itemToCheck as T).some((key) => propertyNames.indexOf(key as keyof T) >= 0)
         : (itemToCheck as T)[propertyNames as keyof T] !== undefined;
 };
 
+/**
+ * Pluck - allows you to pick one / multiple properties from one object
+ *
+ * @template T
+ * @template K
+ * @param {T} o
+ * @param {(K[] | K)} propertyNames
+ * @returns {(T[K][] | T[K])}
+ */
 export const pluck = <T, K extends keyof T>(o: T, propertyNames: K[] | K): T[K][] | T[K] => {
     return Array.isArray(propertyNames) ? propertyNames.map((n) => o[n]) : o[propertyNames];
 };
