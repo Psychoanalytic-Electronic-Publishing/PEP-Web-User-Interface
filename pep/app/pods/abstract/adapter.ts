201a23ecafb1aac9079bf176b9474c0d8015085b
Bill Dami <bdami@gavant.com>
2020-08-25 09:24:07 -0400

    cleanup abstract model adapter

--- pep/app/pods/abstract/adapter.ts
+++ pep/app/pods/abstract/adapter.ts
@@ -1,23 +1,20 @@
 import { classify } from '@ember/string';
-import DS from 'ember-data';
 import { pluralize } from 'ember-inflector';
 
+import ENV from 'pep/config/environment';
 import ApplicationAdapter from 'pep/pods/application/adapter';
 
 export default class Abstract extends ApplicationAdapter {
+    namespace = `${ENV.apiNamespace}/Documents`;
+
     /**
-     * The endpoint for individual abstracts is /v2/Documents/Abstracts/{id}
-     * @param {String} id
-     * @param {String | Number} modelName
-     * @param {DS.Snapshot<K>} snapshot
-     * @returns {String}
+     * Customize the default model endpoint path
+     * The endpoint for individual abstracts is /v2/Documents/Abstracts/{id}/
+     * @param {String} modelName
      */
-    urlForFindRecord<K extends string | number>(id: string, modelName: K, snapshot: DS.Snapshot<K>): string {
-        const modelNameStr = modelName.toString();
-        const origPathSegment = pluralize(classify(modelNameStr));
-        const newPathSegment = `Documents/${origPathSegment}`;
-        const url = super.urlForFindRecord(id, modelName, snapshot);
-        return url.replace(`/${origPathSegment}`, `/${newPathSegment}`);
+    pathForType<K extends string | number>(modelName: K) {
+        const path = super.pathForType(modelName);
+        return pluralize(classify(path));
     }
 }
 
