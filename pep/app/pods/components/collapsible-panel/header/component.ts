de58b5f59aba7d78d98f09a3f311cc152607bf35
Bill Dami <billdami@gmail.com>
2020-08-27 13:32:36 -0400

    refactor of collapsible panel header actions

--- pep/app/pods/components/collapsible-panel/header/component.ts
+++ pep/app/pods/components/collapsible-panel/header/component.ts
@@ -2,6 +2,11 @@ import Component from '@glimmer/component';
 
 interface CollapsiblePanelHeaderArgs {
     title: string;
+    secondaryAction?: {
+        action: () => void;
+        icon: string;
+        label: string;
+    };
 }
 
 export default class CollapsiblePanelHeader extends Component<CollapsiblePanelHeaderArgs> {}
