774d98344663d8188f4a74aefbc1e291cf40eb19
Adam Baker <bakerac4@gmail.com>
2020-08-31 12:27:59 -0400

    Updates to about modal

--- pep/app/pods/components/modal-dialogs/user/about/component.ts
+++ pep/app/pods/components/modal-dialogs/user/about/component.ts
@@ -1,12 +1,28 @@
 import Component from '@glimmer/component';
 import { ServerStatus } from 'pep/pods/application/controller';
+import IntlService from 'ember-intl/services/intl';
+import { inject as service } from '@ember/service';
 
 interface ModalDialogsUserAboutArgs {
     onClose: () => void;
     options: {
-        serverInformation: Promise<ServerStatus>;
+        serverInformation: ServerStatus;
         clientBuildVersion: string;
     };
 }
 
-export default class ModalDialogsUserAbout extends Component<ModalDialogsUserAboutArgs> {}
+export default class ModalDialogsUserAbout extends Component<ModalDialogsUserAboutArgs> {
+    @service intl!: IntlService;
+
+    get isStatusOk() {
+        return this.args.options.serverInformation.db_server_ok && this.args.options.serverInformation.text_server_ok;
+    }
+
+    get status() {
+        if (this.isStatusOk) {
+            return this.intl.t('about.status.ok');
+        } else {
+            return this.intl.t('about.status.down');
+        }
+    }
+}
