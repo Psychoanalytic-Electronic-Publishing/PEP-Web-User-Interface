5ae122aca1f5c534af973b898507b16e0c4b7b57
Adam <bakerac4@gmail.com>
2020-10-15 07:34:19 -0400

    Fixing changes to possiblePeriodValues

--- pep/app/constants/sidebar.ts
+++ pep/app/constants/sidebar.ts
@@ -1,9 +1,9 @@
-export type PossiblePeriodValues = '5' | '10' | '20' | '';
+export type PossiblePeriodValues = '5' | '10' | '20' | 'all';
 export type Period = { translationKey: string; value: PossiblePeriodValues };
 export const PERIOD_5_YEARS: Period = { translationKey: 'mostCited.fiveYears', value: '5' };
 export const PERIOD_10_YEARS: Period = { translationKey: 'mostCited.tenYears', value: '10' };
 export const PERIOD_20_YEARS: Period = { translationKey: 'mostCited.twentyYears', value: '20' };
-export const PERIOD_ALL_YEARS: Period = { translationKey: 'mostCited.allYears', value: '' };
+export const PERIOD_ALL_YEARS: Period = { translationKey: 'mostCited.allYears', value: 'all' };
 
 export const PERIODS = [PERIOD_5_YEARS, PERIOD_10_YEARS, PERIOD_20_YEARS, PERIOD_ALL_YEARS];
 
