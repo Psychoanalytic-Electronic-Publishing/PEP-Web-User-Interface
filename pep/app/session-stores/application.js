37e5166c4230b73d9b58ee36d0031ee3189303fe
Adam <bakerac4@gmail.com>
2020-10-23 12:38:31 -0400

    Trying to fix login issues

--- pep/app/session-stores/application.js
+++ pep/app/session-stores/application.js
@@ -3,7 +3,6 @@ import ENV from 'pep/config/environment';
 
 export default CookieStore.extend({
     cookieName: 'pepweb_session',
-    cookiePath: null,
     cookieExpirationTime: 60 * 60 * 24 * 365, // 1 year
     sameSite: ENV.cookieSameSite
 });
