b5cbb1735b764ef5e8befa1111004672214531ca
Adam Baker <bakerac4@gmail.com>
2020-12-02 09:12:17 -0500

    Fixes for MR

--- pep/app/pods/components/modal-dialogs/user/preferences/component.ts
+++ pep/app/pods/components/modal-dialogs/user/preferences/component.ts
@@ -2,6 +2,9 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 
+import { timeout } from 'ember-concurrency';
+import { restartableTask } from 'ember-concurrency-decorators';
+import { taskFor } from 'ember-concurrency-ts';
 import IntlService from 'ember-intl/services/intl';
 
 import { PreferenceKey, UserPreferences } from 'pep/constants/preferences';
@@ -44,12 +47,27 @@ export default class ModalDialogsUserPreferences extends Component<ModalDialogsU
      * @param {PreferenceKey} key
      * @param {String} newThemeId
      */
+    @restartableTask
+    *updatePreferenceTask<K extends PreferenceKey>(key: K, value: UserPreferences[K]) {
+        yield timeout(250);
+        yield this.currentUser.updatePrefs({ [key]: value });
+    }
+
+    /**
+     * Calls the update preference task to debounce save
+     *
+     * @template K
+     * @param {K} key
+     * @param {(UserPreferences[K] | InputEvent)} value
+     * @return {*}
+     * @memberof ModalDialogsUserPreferences
+     */
     @action
     updatePreference<K extends PreferenceKey>(key: K, value: UserPreferences[K] | InputEvent) {
         let newValue = value;
         if (guard<InputEvent>(newValue, 'data')) {
             newValue = newValue.data as UserPreferences[K];
         }
-        this.currentUser.updatePrefs({ [key]: newValue });
+        return taskFor(this.updatePreferenceTask).perform(key, newValue);
     }
 }
