43640b6149617630cdbfd578c7ca93fa95d044fb
Adam Baker <bakerac4@gmail.com>
2020-12-15 12:29:44 -0500

    Cleanup for MR

--- pep/app/pods/components/head-layout/component.ts
+++ pep/app/pods/components/head-layout/component.ts
@@ -1,6 +1,7 @@
 import { inject as service } from '@ember/service';
 
 import FastbootService from 'ember-cli-fastboot/services/fastboot';
+// @ts-ignore
 import HeadLayout from 'ember-cli-head/components/head-layout';
 
 export default class HeadLayoutTest extends HeadLayout {
--- pep/tests/integration/pods/components/head-layout/component-test.ts
+++ pep/tests/integration/pods/components/head-layout/component-test.ts
@@ -1,26 +1,19 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
+
+import { setupRenderingTest } from 'ember-qunit';
+
 import hbs from 'htmlbars-inline-precompile';
+import { module, test } from 'qunit';
 
 module('Integration | Component | head-layout', function(hooks) {
-  setupRenderingTest(hooks);
+    setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    test('it renders', async function(assert) {
+        // Set any properties with this.set('myProperty', 'value');
+        // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{head-layout}}`);
+        await render(hbs`{{head-layout}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#head-layout}}
-        template block text
-      {{/head-layout}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
-  });
+        assert.equal(this.element.textContent?.trim(), '');
+    });
 });
