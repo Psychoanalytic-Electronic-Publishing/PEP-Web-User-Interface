8f66d8b29f430ae4b3c58529f2f4e3f3d9f82d51
Nick Pafundi <npafundi@gavant.com>
2020-12-29 17:46:57 -0500

    finish feedback form

--- pep/app/pods/components/modal-dialogs/help/feedback/component.ts
+++ pep/app/pods/components/modal-dialogs/help/feedback/component.ts
@@ -1,7 +1,14 @@
-import { ModelChangeset } from '@gavant/ember-validations/utilities/create-changeset';
-import Component from '@glimmer/component';
-import { inject as service } from '@ember/service';
+import NotificationService from 'ember-cli-notifications/services/notifications';
+import IntlService from 'ember-intl/services/intl';
 import UserAgentService from 'ember-useragent/services/user-agent';
+import AjaxService from 'pep/services/ajax';
+import LoadingBarService from 'pep/services/loading-bar';
+
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import createChangeset, { ModelChangeset } from '@gavant/ember-validations/utilities/create-changeset';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
 interface Feedback {
     subject: string;
@@ -13,8 +20,51 @@ interface Feedback {
 
 type FeedbackChangeset = ModelChangeset<Feedback>;
 
-interface ModalDialogsHelpFeedbackArgs {}
+interface ModalDialogsHelpFeedbackArgs {
+    onClose: () => void;
+}
 
 export default class ModalDialogsHelpFeedback extends Component<ModalDialogsHelpFeedbackArgs> {
     @service userAgent!: UserAgentService;
+    @service loadingBar!: LoadingBarService;
+    @service notifications!: NotificationService;
+    @service intl!: IntlService;
+    @service ajax!: AjaxService;
+
+    @tracked changeset!: FeedbackChangeset;
+
+    constructor(owner: unknown, args: ModalDialogsHelpFeedbackArgs) {
+        super(owner, args);
+        const feedbackChangeset = createChangeset<Feedback>(
+            {
+                subject: '',
+                description: '',
+                url: window.location.href,
+                type: '',
+                browser: this.userAgent.browser.info
+            },
+            {}
+        );
+        this.changeset = feedbackChangeset;
+    }
+
+    @action
+    submit(changeset: FeedbackChangeset) {
+        try {
+            this.loadingBar.show();
+            changeset.execute();
+            const results = this.ajax.request('', {
+                method: 'POST',
+                body: this.ajax.stringifyData(changeset.data)
+            });
+            this.notifications.success(this.intl.t('feedback.feedbackSuccessful'));
+            this.loadingBar.hide();
+            this.args.onClose();
+            return results;
+        } catch (errors) {
+            this.loadingBar.hide();
+            this.notifications.error(errors);
+            throw errors;
+        }
+    }
 }
--- pep/tests/integration/pods/components/modal-dialogs/help/feedback/component-test.ts
+++ pep/tests/integration/pods/components/modal-dialogs/help/feedback/component-test.ts
@@ -4,23 +4,14 @@ import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | modal-dialogs/help/feedback', function(hooks) {
-  setupRenderingTest(hooks);
+    setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    test('it renders', async function(assert) {
+        // Set any properties with this.set('myProperty', 'value');
+        // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{modal-dialogs/help/feedback}}`);
+        await render(hbs`<ModalDialogs::Help::Feedback />`);
 
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#modal-dialogs/help/feedback}}
-        template block text
-      {{/modal-dialogs/help/feedback}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
-  });
+        assert.ok(this.element);
+    });
 });
