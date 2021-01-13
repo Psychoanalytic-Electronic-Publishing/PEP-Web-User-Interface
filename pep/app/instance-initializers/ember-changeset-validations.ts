c707578e2b5a4a455b918afc55463f9376f2fd02
Nick Pafundi <npafundi@gavant.com>
2021-01-13 09:00:16 -0500

    override messages() method for translation of validations

---
+++ pep/app/instance-initializers/ember-changeset-validations.ts
@@ -0,0 +1,24 @@
+import ApplicationInstance from '@ember/application/instance';
+import Messages from 'ember-changeset-validations/utils/messages';
+import { isNone } from '@ember/utils';
+
+/**
+ * Override Ember-Validators exported `formatMessage()` method to allow
+ * for translation of validation messages.
+ *
+ * @export
+ * @param {ApplicationInstance} applicationInstance
+ */
+export function initialize(applicationInstance: ApplicationInstance) {
+    const intl = applicationInstance.lookup('service:intl');
+    Messages.formatMessage = (message: string, context = {}) => {
+        let errorKey = message;
+        if (isNone(errorKey) || typeof errorKey !== 'string') {
+            return intl.t('validations.invalid');
+        }
+        return intl.t(`validations.${errorKey}`, context);
+    };
+}
+export default {
+    initialize
+};
