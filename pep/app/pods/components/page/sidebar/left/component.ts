016286863bb162414b0380c4581a120dd41d23cf
Adam Baker <bakerac4@gmail.com>
2021-03-10 15:05:54 -0500

    Cleanup for MR

--- pep/app/pods/components/page/sidebar/left/component.ts
+++ pep/app/pods/components/page/sidebar/left/component.ts
@@ -4,9 +4,7 @@ import Component from '@glimmer/component';
 import { WidgetData } from 'pep/constants/sidebar';
 import SidebarService from 'pep/services/sidebar';
 
-interface PageSidebarLeftArgs {
-    data: WidgetData;
-}
+interface PageSidebarLeftArgs {}
 
 export default class PageSidebarLeft extends Component<PageSidebarLeftArgs> {
     @service sidebar!: SidebarService;
