7510b551fc38bfde273b92cbc4f7167095b48c35
Adam Baker <bakerac4@gmail.com>
2021-03-15 07:52:46 -0400

    Cleanup for MR

--- pep/app/pods/admin/language/route.ts
+++ pep/app/pods/admin/language/route.ts
@@ -34,7 +34,6 @@ export default class AdminLanguage extends Route {
     setupController(controller: AdminLanguageController, model: Configuration, transition: Transition): void {
         super.setupController(controller, model, transition);
         model.configSettings = merge(DEFAULT_CONTENT_CONFIGURATION, model.configSettings);
-        // model.configSettings = DEFAULT_CONTENT_CONFIGURATION;
         controller.changeset = createChangeset(model, CONFIGURATION_LANGUAGE_VALIDATIONS);
         const langId = model.configName as LanguageCode;
         controller.language = Languages.findBy('code', langId);
