133385a87384219e519d9aa6864653cff2646edc
Adam Baker <bakerac4@gmail.com>
2021-04-30 10:43:36 -0400

    Putting width back to default

--- pep/app/constants/dimensions.ts
+++ pep/app/constants/dimensions.ts
@@ -1,6 +1,6 @@
 export const NAV_BAR_HEIGHT = 58;
 export const FOOT_BAR_HEIGHT = 40;
-export const SIDEBAR_WIDTH = 315;
+export const SIDEBAR_WIDTH = 300;
 export const SIDEBAR_HANDLE_WIDTH = 16;
 export const SIDEBAR_MAX_WIN_PCT_DESKTOP = 0.35;
 export const SIDEBAR_MAX_WIN_PCT_TABLET = 0.5;
