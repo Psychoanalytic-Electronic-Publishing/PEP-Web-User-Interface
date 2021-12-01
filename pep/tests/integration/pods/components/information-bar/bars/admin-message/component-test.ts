3e3f91cb8ec4dac704c2b48fd027aea94888f29c
Adam Baker <bakerac4@gmail.com>
2021-12-01 10:32:36 -0500

    Fixing tests

--- pep/tests/integration/pods/components/information-bar/bars/admin-message/component-test.ts
+++ pep/tests/integration/pods/components/information-bar/bars/admin-message/component-test.ts
@@ -1,26 +1,19 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
+
+import { setupRenderingTest } from 'ember-qunit';
+
 import hbs from 'htmlbars-inline-precompile';
+import { module, test } from 'qunit';
 
 module('Integration | Component | information-bar/bars/admin-message', function(hooks) {
-  setupRenderingTest(hooks);
+    setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    test('it renders', async function(assert) {
+        // Set any properties with this.set('myProperty', 'value');
+        // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{information-bar/bars/admin-message}}`);
+        await render(hbs`{{information-bar/bars/admin-message}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#information-bar/bars/admin-message}}
-        template block text
-      {{/information-bar/bars/admin-message}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
-  });
+        assert.equal(this.element.textContent?.trim(), '');
+    });
 });
