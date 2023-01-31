76b0f40042c1a28480d47acc7a0ca5170c365c31
Jordan Allen <jordan@jordanallen.org>
2023-01-31 15:21:02 +0000

    Initialise Terraform with AWS and S3 backend config
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

---
+++ infra/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "s3" {
+    key = "global/s3/pep-web-stage.tfstate"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
