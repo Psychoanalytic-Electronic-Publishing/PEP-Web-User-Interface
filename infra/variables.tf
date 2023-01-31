faf9b7ec8971dc8038f9a77effc89c71460a7409
Jordan Allen <jordan@jordanallen.org>
2023-01-31 15:36:03 +0000

    Define bucket with public access and CORS policy
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,3 +2,23 @@ variable "aws_region" {
   description = "AWS region"
   default     = "us-east-1"
 }
+
+variable "domain_name" {
+  description = "Domain name"
+  default     = "pep-web.org"
+}
+
+variable "env" {
+  description = "Environment name"
+  type        = string
+  default     = "staging"
+}
+
+variable "stack_name" {
+  description = "Root name for the stack"
+  default     = "pep-web"
+}
+
+locals {
+  assets_domain = "assets-stage.${var.domain_name}"
+}
