e9547547263b91cb6026931fcebe2e00792b1f27
Jordan Allen <jordan@jordanallen.org>
2023-02-01 16:07:48 +0000

    Define fastboot Lambda
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,11 +1,24 @@
-data "aws_lambda_function" "fastboot" {
-  function_name = "pep-fastboot-staging-web-launchFastboot"
+module "fastboot_lambda" {
+  source = "terraform-aws-modules/lambda/aws"
+
+  function_name          = "${var.stack_name}-handler-${var.env}"
+  source_path            = "../pep/node"
+  handler                = "lambda.handler"
+  runtime                = "nodejs16.x"
+  timeout                = 29
+  memory_size            = 1024
+  ephemeral_storage_size = 512
+
+  tags = {
+    stage = var.env
+    stack = var.stack_name
+  }
 }
 
-resource "aws_lambda_permission" "allow_api" {
-  statement_id  = "${var.stack_name}-allow-api}"
+resource "aws_lambda_permission" "allow_api_2" {
+  statement_id  = "${var.stack_name}-allow-api-${var.env}-temp"
   action        = "lambda:InvokeFunction"
-  function_name = data.aws_lambda_function.fastboot.function_name
+  function_name = module.fastboot_lambda.lambda_function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/*/*"
 }
