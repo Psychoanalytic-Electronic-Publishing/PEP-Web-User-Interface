e84973e2cba1829901bde206ee427ceb6eae7d53
Jordan Allen <jordan@jordanallen.org>
2023-02-02 22:05:35 +0000

    Update minimum_protocol_version
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- infra/cloudfront.tf
+++ infra/cloudfront.tf
@@ -58,6 +58,6 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   viewer_certificate {
     acm_certificate_arn      = aws_acm_certificate_validation.validation.certificate_arn
     ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2021"
+    minimum_protocol_version = "TLSv1.2_2019"
   }
 }
