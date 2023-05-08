246020abe7e49082e48d29b99b30d7de91519116
Jordan Allen <jordan@jordanallen.org>
2023-05-08 13:13:27 +0100

    Block all public access
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- infra/video/bucket.tf
+++ infra/video/bucket.tf
@@ -10,10 +10,10 @@ resource "aws_s3_bucket" "video_previews" {
 resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket = aws_s3_bucket.video_previews.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_cors_configuration" "cors" {
