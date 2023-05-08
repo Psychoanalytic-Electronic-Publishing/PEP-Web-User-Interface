5f84bd9e029a2873b4089f1bc44cbfd61f16b315
Jordan Allen <jordan@jordanallen.org>
2023-05-08 13:20:56 +0100

    Grant permission for Cloudfront to access S3
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- infra/video/bucket.tf
+++ infra/video/bucket.tf
@@ -26,3 +26,36 @@ resource "aws_s3_bucket_cors_configuration" "cors" {
     expose_headers  = []
   }
 }
+
+
+resource "aws_s3_bucket_policy" "allow_access_from_cloudfront" {
+  bucket = aws_s3_bucket.video_previews.id
+  policy = data.aws_iam_policy_document.allow_access_from_cloudfront.json
+}
+
+data "aws_iam_policy_document" "allow_access_from_cloudfront" {
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      aws_s3_bucket.video_previews.arn,
+      "${aws_s3_bucket.video_previews.arn}/*",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+
+      values = [
+        aws_cloudfront_distribution.s3_distribution.arn
+      ]
+    }
+  }
+}
