952a0c6cc2dbf9ca9b050dbf1e22c0156d30de94
Jordan Allen <jordan@jordanallen.org>
2023-12-07 14:55:30 +0000

    Define placeholder modal for SiD registration prompt
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

---
+++ pep/app/pods/components/modal-dialogs/user/sid/component.ts
@@ -0,0 +1,28 @@
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import Component from '@glimmer/component';
+import { LoginMethod } from 'pep/constants/user';
+
+import CurrentUserService from 'pep/services/current-user';
+import PepSessionService from 'pep/services/pep-session';
+import { BaseGlimmerSignature } from 'pep/utils/types';
+
+interface ModalDialogsSidArgs {
+    onClose: () => void;
+}
+
+export default class ModalDialogsUserInfo extends Component<BaseGlimmerSignature<ModalDialogsSidArgs>> {
+    @service currentUser!: CurrentUserService;
+    @service('pep-session') session!: PepSessionService;
+
+    /**
+     * Open the PaDS home-site using the user's generated
+     * link in a new tab.
+     */
+    @action
+    openPadsTab() {
+        if (this.currentUser.user?.paDSHomeURL) {
+            window.open(this.currentUser.user.paDSHomeURL, '_blank');
+        }
+    }
+}
