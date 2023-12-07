58581a223b6aabde3321d655b6cec22d6aff987c
Jordan Allen <jordan@jordanallen.org>
2023-12-07 15:33:19 +0000

    Fetch and open reigstration URL
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- pep/app/pods/components/modal-dialogs/user/sid/component.ts
+++ pep/app/pods/components/modal-dialogs/user/sid/component.ts
@@ -1,11 +1,14 @@
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
-import { LoginMethod } from 'pep/constants/user';
+import ENV from 'pep/config/environment';
 
 import CurrentUserService from 'pep/services/current-user';
 import PepSessionService from 'pep/services/pep-session';
+import AuthService, { FederatedLoginResponse } from 'pep/services/auth';
 import { BaseGlimmerSignature } from 'pep/utils/types';
+import { serializeQueryParams } from 'pep/utils/url';
+import AjaxService from 'pep/services/ajax';
 
 interface ModalDialogsSidArgs {
     onClose: () => void;
@@ -14,15 +17,27 @@ interface ModalDialogsSidArgs {
 export default class ModalDialogsUserInfo extends Component<BaseGlimmerSignature<ModalDialogsSidArgs>> {
     @service currentUser!: CurrentUserService;
     @service('pep-session') session!: PepSessionService;
+    @service auth!: AuthService;
+    @service ajax!: AjaxService;
 
     /**
      * Open the PaDS home-site using the user's generated
      * link in a new tab.
      */
     @action
-    openPadsTab() {
-        if (this.currentUser.user?.paDSHomeURL) {
-            window.open(this.currentUser.user.paDSHomeURL, '_blank');
+    async openPadsTab() {
+        const session = this.session.isAuthenticated
+            ? this.session.data?.authenticated?.SessionId
+            : this.session.getUnauthenticatedSession()?.SessionId;
+        const params = serializeQueryParams({
+            sessionId: session ?? ''
+        });
+        const federatedLogins = await this.ajax.request<FederatedLoginResponse>(`${ENV.federatedLoginUrl}?${params}`, {
+            appendTrailingSlash: false
+        });
+
+        if (federatedLogins.PaDSRegisterUserURL) {
+            window.open(federatedLogins.PaDSRegisterUserURL, '_blank');
         }
     }
 }
