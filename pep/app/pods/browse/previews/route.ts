a52b516b43e89284fc05c7b59df7dca5997469fb
Jordan Allen <jordan@jordanallen.org>
2024-02-23 23:10:18 +0000

    Render first page of preview results as list
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

---
+++ pep/app/pods/browse/previews/route.ts
@@ -0,0 +1,26 @@
+import Route from '@ember/routing/route';
+
+import { buildSearchQueryParams } from 'pep/utils/search';
+
+export default class BrowsePreviews extends Route {
+    /**
+     * Fetch the preview documents
+     */
+    model() {
+        const queryParams = buildSearchQueryParams({
+            smartSearchTerm: 'file_classification:preview'
+        });
+
+        return this.store.query('search-document', queryParams, {
+            adapterOptions: {
+                openUrl: true
+            }
+        });
+    }
+
+    afterModel(model: any) {
+        for (const doc of model.toArray()) {
+            console.log(doc);
+        }
+    }
+}
