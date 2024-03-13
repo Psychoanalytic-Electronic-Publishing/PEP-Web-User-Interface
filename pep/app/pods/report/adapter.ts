8f94c584f82ea3a018d377d6686a7c3b5babef74
Jordan Allen <jordan@jordanallen.org>
2024-03-13 13:08:36 +0000

    Define report adapter
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

---
+++ pep/app/pods/report/adapter.ts
@@ -0,0 +1,31 @@
+import ENV from 'pep/config/environment';
+import Application from 'pep/pods/application/adapter';
+
+export default class ReportAdapter extends Application {
+    async downloadReport(reportType: string) {
+        let url = `${ENV.apiBaseUrl}/v2/Admin/Reports/${reportType}?`;
+        const params = new URLSearchParams({
+            download: 'true',
+            sortorder: 'ASC',
+            limit: '1000',
+            offset: '0'
+        });
+
+        const response = await fetch(url + params, {
+            method: 'GET',
+            headers: this.headers
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        return response.text();
+    }
+}
+
+declare module 'ember-data/types/registries/adapter' {
+    export default interface AdapterRegistry {
+        report: ReportAdapter;
+    }
+}
