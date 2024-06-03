805073eaa2176871ba87e11074740d7a7d892acf
Jordan Allen <jordan@jordanallen.org>
2024-06-03 14:22:57 +0100

    Upgrade to GA4 and track DOI click
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- pep/config/environment.js
+++ pep/config/environment.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function(environment) {
+module.exports = function (environment) {
     let ENV = {
         modulePrefix: 'pep',
         podModulePrefix: 'pep/pods',
@@ -29,7 +29,7 @@ module.exports = function(environment) {
 
         metricsAdapters: [
             {
-                name: 'GoogleAnalytics',
+                name: 'GoogleAnalyticsFour',
                 environments: ['development', 'production'],
                 config: {
                     id: process.env.GOOGLE_ANALYTICS_ID,
