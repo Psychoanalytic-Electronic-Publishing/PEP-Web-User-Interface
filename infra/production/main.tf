011c9f1af2fe39854399525b2d02190a6b9b3813
Jordan Allen <jordan@jordanallen.org>
2026-04-29 21:11:55 +0100

    Disable custom domain control in prod to avoid conflcit during cutover
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- infra/production/main.tf
+++ infra/production/main.tf
@@ -38,6 +38,8 @@ module "web_server" {
   build_version      = var.build_version
   kms_key_arn        = "arn:aws:kms:us-east-1:547758924192:key/7b802a2f-38b3-40af-bca1-cdbc45ad8ceb"
   pep_waf_key        = var.pep_waf_key
+  # Disabled while prod.pep-web.org is managed by an infra POC in another repo.
+  enable_custom_domain = false
 }
 
 resource "aws_sns_topic" "translation_updates" {
